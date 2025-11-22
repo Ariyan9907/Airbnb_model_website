8eb65a5597d44e459e7310ceb5f8e99ad2b34dda
ARIYAN MARUTI PUJARI <iampujari1@gmail.com>
2025-11-22 18:25:54 +0530

    added review sections

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
 const path=require("path");
-const Listing=require("./models/listing.js");
-const { title } = require("process");
 const methodOverride = require('method-override');
 const ejsMate=require("ejs-mate");
-const wrapAsync=require("./utils/wrapAsync.js");
 const expressError=require("./utils/expressError.js");
-const {listingSchema,reviewSchema}=require("./schema.js");
-const Review=require("./models/review.js");
+
+const listings=require("./routes/listing.js");
+const reviews=require("./routes/review.js");
+
+
 
 port=3000;
 
@@ -23,27 +23,6 @@ app.use(express.static(path.join(__dirname,"/public")));
 app.engine('ejs', ejsMate);
 
 
-const validateListing=(req,res,next)=>{
-    let{error}=listingSchema.validate(req.body);
-    if(error){
-        let errMsg=error.details.map((el)=>el.message).join(",");
-        throw new expressError(400,errMsg);
-    }
-    else{
-        next();
-    }
-}
-
-const validateReview=(req,res,next)=>{
-    let{error}=reviewSchema.validate(req.body);
-    if(error){
-        let errMsg=error.details.map((el)=>el.message).join(",");
-        throw new expressError(400,errMsg);
-    }
-    else{
-        next();
-    }
-}
 
 main().then(()=>{
     console.log("Connection to database successfull");
@@ -55,83 +34,14 @@ async function main() {
 
 app.get("/",(req,res)=>{
     res.send("All the rotes working and you are at root");
-})
-
-//index route
-app.get("/listings",wrapAsync(async(req,res)=>{
-    const allListings=await Listing.find({});
-    res.render("listings/index.ejs",{allListings});
-}));
-
-//new route
-app.get("/listings/new",(req,res)=>{
-    res.render("listings/new.ejs")
 });
 
+//for the routes of listing
+app.use("/listings",listings);
 
+//for the routes of reviews
+app.use("/listings/:id/reviews",reviews);
 
-//show route
-app.get("/listings/:id",wrapAsync(async(req,res)=>{
-    let {id}=req.params;
-    const listing=await Listing.findById(id).populate("reviews");
-    res.render("listings/show.ejs",{listing});
-    
-}));
-
-//create route
-app.post("/listings",validateListing,wrapAsync(async(req,res)=>{
-    // let{title,description,image,price,location,country}=req.body;
-    const newListing= new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
-
-}));
-
-//edit route
-app.get("/listings/:id/edit",wrapAsync(async(req,res)=>{
-     let {id}=req.params;
-    const listing=await Listing.findById(id);
-    res.render("listings/edit.ejs",{listing});
-}));
-//uddate route
-app.put("/listings/:id/edit",validateListing,wrapAsync(async(req,res)=>{
-    let {id}=req.params;
-    await Listing.findByIdAndUpdate(id,{...req.body.listing});
-    res.redirect("/listings");
-}));
-
-//destroy route
-app.delete("/listings/:id",wrapAsync(async(req,res)=>{
-    let {id}=req.params;
-    await Listing.findByIdAndDelete(id);
-    res.redirect("/listings");
-}));
-
-//Reviews
-//post review route
-app.post("/listings/:id/reviews",validateReview,wrapAsync(async(req,res)=>{
-    let listing=await Listing.findById(req.params.id);
-    let newReview=new Review(req.body.review);
-    console.log(newReview);
-
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-
-    console.log("new review saved");
-    res.redirect(`/listings/${listing._id}`)
-}));
-
-//delete review route
-app.delete("/listings/:id/reviews/:reviewId",wrapAsync(async(req,res)=>{
-    let{id,reviewId}=req.params;
-
-    await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}})
-    await Review.findByIdAndDelete(reviewId);
-
-    res.redirect(`/listings/${id}`);
-}))
 
 
 // Catch everything using a path that won't be parsed as a malformed token
